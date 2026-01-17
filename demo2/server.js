3f04f34067d15b994f2a8d8008d86354d79d62df
little-zio <z5509919@ad.unsw.edu.au>
2026-01-17 23:54:24 +0800

    connect to mysql

---
+++ demo2/server.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: "demo1"
+})
+
+connection.connect();
+
+connection.query('select * from user', (err, results, fields) => {
+    if (err) throw err;
+    console.log(results);
+});
+
+connection.end();
+
+
+
+
+
+
+
