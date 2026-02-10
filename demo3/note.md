a9c92954922d4a5acf4c88a6fa43524cca74e649
little-zio <z5509919@ad.unsw.edu.au>
2026-02-10 16:10:56 +1100

    add mysql select & insert

--- demo3/note.md
+++ demo3/note.md
@@ -4,13 +4,16 @@
 
 
 
-
+<!-- ?就是占位符，用于防止SQL注入 -->
 connection.query('select * from 表名称 where userName=? and userPwd=?', [第一个？,第二个？], (err, results, fields) => {}
 )
 
+2. mysql 插入数据
+    insert into 表名称 values (?, ?)
+connection.query('insert into 表名称 values (?, ?)', [第一个？,第二个？], (err, results, fields) => {}
+)
 
-
-
+自增长的键值用 0 [0, userName, userPwd]
 
 
 
--- demo3/server.js
+++ demo3/server.js
@@ -4,33 +4,43 @@ const mysql = require('mysql2');
 
 const server = http.createServer((req, res) => {
 
-    let postVal ="";
-    req.on("data", (chunk) => {
-        postVal += chunk;
+    if (req.url != '/favicon.ico') {  // 过滤掉浏览器自动请求的 favicon.ico 
+    
+        let postVal ="";
+        req.on("data", (chunk) => {
+            postVal += chunk;
     })
 
     req.on("end", ()=>{
         let formVal = querystring.parse(postVal);
         let userName = formVal.userName;
         let userPwd = formVal.userPwd;
-
+        
         const connection = mysql.createConnection({
             host: "localhost",
             user: "root",
             password: "root123",
             database: "demo1"
         })
-
+        
         connection.connect();
-
-        connection.query("select * from user", (err, results, fields)=>{
+        
+        connection.query("select * from user where userName=? and userPwd=?", [userName, userPwd], (err, results, fields)=>{
             if(err) throw err;
-            
-
+            if(results.length > 0){
+                res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
+                res.write('登录成功');
+                res.end();
+            } else {
+                res.end("登录失败");
+            }
         })
-
-        res.end();
+        connection.end();
+        
     })
+    
+    }
+
 })
 
 server.listen(8080);
